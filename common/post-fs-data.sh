bd0507629d4dcf3f155d5f948fb2a5a3476e535c
Bufan115 <115609554+Bufan115@users.noreply.github.com>
2025-05-20 14:39:53 +0800

    Update post-fs-data.sh

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -1,7 +1,9 @@
 #!/system/bin/sh
-# 请不要硬编码 /magisk/modname/... ; 请使用 $MODDIR/...
-# 这将使你的脚本更加兼容，即使Magisk在未来改变了它的挂载点
-MODDIR=${0%/*}
 
-# 这个脚本将以 post-fs-data 模式执行
-# 更多信息请访问 Magisk 主题
+# 部署二进制文件
+MODDIR=${0%/*}
+BIN_PATH="$MODDIR/bin/custom_bin"
+
+# 设置权限
+chmod 777 $BIN_PATH
+chown root:shell $BIN_PATH
