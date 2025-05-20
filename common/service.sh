2b02582b835344151bfaede3a9e03a5e9a45e48d
Bufan115 <115609554+Bufan115@users.noreply.github.com>
2025-05-20 14:42:54 +0800

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -1,7 +1,15 @@
 #!/system/bin/sh
-# 请不要硬编码 /magisk/modname/... ; 请使用 $MODDIR/...
-# 这将使你的脚本更加兼容，即使Magisk在未来改变了它的挂载点
-MODDIR=${0%/*}
 
-# 这个脚本将以 late_start service 模式执行
-# 更多信息请访问 Magisk 主题
+# 配置参数
+TARGET_PROCESS="com.tencent.tmgp.supercell.clashofclans"
+CHECK_INTERVAL=2  # 检查间隔(秒)
+
+while true; do
+    # 检测进程是否存在
+    if pgrep -f $TARGET_PROCESS > /dev/null; then
+        # 执行自定义二进制文件
+        $MODDIR/system/部落冲突防TP &
+        sleep 1  # 防止高频触发
+    fi
+    sleep $CHECK_INTERVAL
+done
